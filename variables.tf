c311028368e3b2da11786bd49212b316b0d3d9f3
Zach Perkins <zachperkins@Zachs-MacBook-Air-3.local>
2025-11-12 19:34:44 -0500

    setup terraform

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "tag_class" {
 
 variable "tag_instructor" {
   type    = string
-  default = "cford38"
+  default = "cford33"
 }
 
 
@@ -37,7 +37,7 @@ variable "environment" {
 variable "student_name" {
   description = "Application Name"
   type        = string
-  default     = "cford38"
+  default     = "zperk"
 }
 
 variable "class_name" {
